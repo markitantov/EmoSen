944fd865fbf52d351ad17fe9582a6ceda82cfab2
Maxim Markitantov <m.markitantov@yandex.ru>
2024-07-12 14:19:26 +0300

    Changed gitignore, added default config

---
+++ src/default_config.py
@@ -0,0 +1,30 @@
+import sys
+
+sys.path.append('src')
+
+data_config: dict = {
+    'CMUMOSEI': {
+        'DATA_ROOT': '',
+        'VIDEO_ROOT': '',
+        'AUDIO_ROOT': '',
+        'VOCALS_ROOT': '',
+        'VAD_FILE': '',
+        'LABELS_FILE': '',
+    },
+    'MELD': {
+        'DATA_ROOT': '',
+        'VIDEO_ROOT': '',
+        'AUDIO_ROOT': '',
+        'VOCALS_ROOT': '',
+        'VAD_FILE': '',
+        'LABELS_FILE': '',
+    },
+    'RAMAS': {
+        'DATA_ROOT': '',
+        'VIDEO_ROOT': '',
+        'AUDIO_ROOT': '',
+        'VOCALS_ROOT': '',
+        'VAD_FILE': '',
+        'LABELS_FILE': '',
+    },  
+}
