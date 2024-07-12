872afc9b5d586c7f59b7a4120290424350c6437c
Maxim Markitantov <m.markitantov@yandex.ru>
2024-07-12 14:15:19 +0300

    Created scripts for audio preprocessing

---
+++ src/run_video_to_audio.py
@@ -0,0 +1,147 @@
+"""
+This is the script for extracting audio from video with/without filtering speech.
+"""
+
+import os
+import wave
+import shutil
+import subprocess
+
+import sox
+from tqdm import tqdm
+
+
+from config import data_config
+
+
+def convert_without_filtering(inp_path: str, 
+                              out_path: str, 
+                              checking: bool = True) -> None:
+    """Convert video to audio using ffmpeg
+
+    Args:
+        inp_path (str): Input file path
+        out_path (str): Output file path
+        checking (bool, optional): Used for checking paths of the ffmpeg command. Defaults to True.
+    """
+    out_dirname = os.path.dirname(out_path)
+    os.makedirs(out_dirname, exist_ok=True)
+
+    # sample rate 16000
+    command = f"ffmpeg -y -i {inp_path} -async 1 -vn -acodec pcm_s16le -ar 16000 -ac 1 {out_path}"
+       
+    if checking:
+        print(command)
+    else:
+        _ = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+
+
+def convert_with_filtering(inp_path: str, 
+                           out_path: str, 
+                           checking: bool = True) -> None:
+    """Extract speech from the video file using Spleeter and ffmpeg
+
+    Args:
+        inp_path (str): Input file path
+        out_path (str): Output file path
+        checking (bool, optional): Used for checking paths of the spleeter/ffmpeg commands. Defaults to True.
+    """
+    out_dirname = os.path.dirname(out_path)
+    os.makedirs(out_dirname, exist_ok=True)
+
+    # 44100 for spleeter
+    command = f"ffmpeg -y -i {inp_path} -async 1 -vn -acodec pcm_s16le -ar 44100 {out_path}"
+       
+    if checking:
+        print(command)
+    else:
+        _ = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+
+    if not checking:
+        inp_duration = sox.file_info.duration(out_path)
+
+    # extract speech using spleeter
+    command = f"spleeter separate -o {out_dirname} {out_path} -d 1620" # maximum length in seconds
+    if checking:
+        print(command)
+    else:
+        _ = subprocess.check_output(
+            command, shell=True, stderr=subprocess.STDOUT, env=os.environ.copy()
+        )
+
+    if not checking:
+        spleeter_duration = sox.file_info.duration(out_path)
+
+    # convert 44100 to 16000
+    command = "ffmpeg -y -i {0} -async 1 -ar 16000 -ac 1 {1}".format(
+        os.path.join(
+            out_dirname, os.path.basename(out_path).split(".")[0], "vocals.wav"
+        ),
+        out_path,
+    )
+    
+    if checking:
+        print(command)
+    else:
+        _ = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+        
+        shutil.rmtree(
+            os.path.join(out_dirname, os.path.basename(out_path).split(".")[0])
+        )
+
+    # check results for errors
+    if not checking:
+        final_duration = sox.file_info.duration(out_path)
+        
+        if (abs(inp_duration - spleeter_duration) < 1e-4) and (abs(inp_duration - final_duration) < 1e-4):
+            pass
+        else:
+            print(f"Error {inp_path}")
+            print(inp_duration, spleeter_duration, final_duration)
+        
+
+def convert_video_to_audio(data_config: dict, 
+                           db: str,
+                           filtering: bool = False,
+                           checking: bool = True) -> None:
+    """Loops through the directory, and extract speech from each video file using Spleeter and ffmpeg.
+
+    Args:
+        data_config (dict): Dictonary info of database
+        db (str): Database: can be 'CMUMOSEI' or 'MELD' or 'RAMAS'
+        filtering (bool, optional): Apply spleeter or not. Defaults to False.
+        checking (bool, optional): Used for checking paths of the spleeter/ffmpeg commands. Defaults to True.
+    """
+    # run on CPU
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    
+    ds_names = ['train', 'dev', 'test'] if db == 'MELD' else ['']
+
+    out_wavs_root = os.path.join(data_config['DATA_ROOT'], data_config['AUDIO_ROOT'])
+    out_vocals_root = os.path.join(data_config['DATA_ROOT'], data_config['VOCALS_ROOT'])
+
+    for ds_name in ds_names:        
+        for fn in tqdm(os.listdir(os.path.join(data_config['VIDEO_ROOT'], ds_name))):
+            if fn.startswith('.'): # corrupted train dia125_utt3
+                continue
+            
+            convert_without_filtering(
+                inp_path=os.path.join(data_config['VIDEO_ROOT'], ds_name, fn),
+                out_path=os.path.join(out_wavs_root, ds_name, fn.replace("mp4", "wav").replace("mov", "wav").replace("avi", "wav")),
+                checking=checking,
+            )
+            
+            if filtering:
+                convert_with_filtering(
+                    inp_path=os.path.join(data_config['VIDEO_ROOT'], ds_name, fn),
+                    out_path=os.path.join(out_vocals_root, ds_name, fn.replace("mp4", "wav").replace("mov", "wav").replace("avi", "wav")),
+                    checking=checking,
+                )
+
+
+if __name__ == "__main__":
+    dbs = list(data_config.keys())
+    
+    for db in dbs:
+        print('Converting {}'.format(db))
+        convert_video_to_audio(data_config=data_config[db], db=db, checking=False, filtering=True)
---
+++ src/run_voice_activity_detector.py
@@ -0,0 +1,72 @@
+"""
+This is the script for extracting audio from video with/without filtering speech.
+"""
+
+import os
+import pickle
+
+import torch
+from tqdm import tqdm
+
+from config import data_config
+
+
+model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
+                              model='silero_vad',
+                              force_reload=True,
+                              onnx=False)
+
+(get_speech_timestamps, _, read_audio, _, _) = utils
+
+
+def detect_speech(inp_path: str,
+                  sampling_rate: int = 16000) -> list[dict]:
+    """Finds speech segments using VAD
+
+    Args:
+        inp_path (str): Input file path
+        sampling_rate (int, optional): Sampling rate of audio. Defaults to 16000.
+
+    Returns:
+        list[dict]: list containing ends and beginnings of speech chunks (samples or seconds based on return_seconds)
+    """
+    
+    wav = read_audio(inp_path, sampling_rate=sampling_rate)
+    # get speech timestamps from full audio file
+    speech_timestamps = get_speech_timestamps(wav, model, sampling_rate=sampling_rate)
+    
+    return speech_timestamps
+
+
+def run_vad(data_config: dict,
+            db: str) -> None:
+    """Loops through the directory, and run VAD on DB.
+
+    Args:
+        data_config (dict): Dictonary info of database
+        db (str): Database: can be 'CMUMOSEI' or 'MELD' or 'RAMAS'
+    """
+    # run on CPU
+    
+    # os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    ds_names = ['train', 'dev', 'test'] if db == 'MELD' else ['']
+    
+    for ds_name in ds_names:
+        res = {} 
+        for fn in tqdm(os.listdir(os.path.join(data_config['DATA_ROOT'], data_config['VOCALS_ROOT'], ds_name))):
+            if fn.startswith('.'): # corrupted train dia125_utt3
+                continue
+            
+            speech_timestamps = detect_speech(inp_path=os.path.join(data_config['DATA_ROOT'], data_config['VOCALS_ROOT'], ds_name, fn))
+            res[fn] = speech_timestamps
+
+        with open(os.path.join(data_config['DATA_ROOT'], 'vad_{0}.pickle'.format(ds_name) if ds_name else 'vad.pickle'), 'wb') as handle:
+            pickle.dump(res, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == "__main__":
+    dbs = list(data_config.keys())
+    
+    for db in dbs:
+        print('Starting VAD on {}'.format(db))
+        run_vad(data_config=data_config[db], db=db)
